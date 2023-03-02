0ef1dfbdc027ca16a669dc0823c5009c40891f74
jerry-1015 <781467164@qq.com>
2023-03-02 16:37:22 +0800

    optimizer

--- lua/doxycpp/init.lua
+++ lua/doxycpp/init.lua
@@ -69,6 +69,7 @@ local function gen_comment()
     if cm_lines ~= nil then
       gen_line_comment(cm_lines, lnum, true)
       api.nvim_win_set_cursor(0, { lnum + 1, #api.nvim_get_current_line() })
+      vim.cmd('startinsert')
     else
       cm_lines = vcomment(lnum, lnum)
       gen_line_comment(cm_lines, lnum, false)
