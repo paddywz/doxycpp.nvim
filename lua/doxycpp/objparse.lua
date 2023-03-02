2b093e15293a8391260580ea9c6cf4a52f21502e
jerry-1015 <781467164@qq.com>
2023-03-02 10:43:33 +0800

    add prefix

--- lua/doxycpp/objparse.lua
+++ lua/doxycpp/objparse.lua
@@ -6,6 +6,7 @@ function M.match(line)
   for _, v in pairs(type_tbl) do
     if line:match("^%s*" .. v) ~= nil then
       M.type = v
+      M.prefix = line:match('^%s*')
       return true
     end
   end
@@ -13,9 +14,9 @@ end
 
 function M.annotation()
   local res = {}
-  table.insert(res, '/** ' .. M.type)
-  table.insert(res, '/* @brief      ')
-  table.insert(res, '**/')
+  table.insert(res, M.prefix .. '/** ' .. M.type)
+  table.insert(res, M.prefix .. '/* @brief      ')
+  table.insert(res, M.prefix .. '**/')
   return res
 end
 
