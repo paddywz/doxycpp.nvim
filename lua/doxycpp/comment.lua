29d57d2e5568159168a068366ed602b3558971e9
jerry-1015 <781467164@qq.com>
2023-03-03 11:42:36 +0800

    modify comment

--- lua/doxycpp/comment.lua
+++ lua/doxycpp/comment.lua
@@ -1,22 +1,29 @@
 local comm = {}
 local fn = vim.fn
 -- add comment
-local function add_comment(line)
-  local newline
-  local _, cur_spaces = line:find('^%s+')
-
-  if cur_spaces == nil then
-    newline = '// ' .. line
-  else
-    newline = line:sub(1, cur_spaces) .. '// ' .. line:sub(cur_spaces + 1, -1)
+local function add_comment(lines, min_spaces)
+  local res = {}
+  for _, v in pairs(lines) do
+    local newline = ""
+    if #v > 0 then
+      newline = string.rep(' ', min_spaces) .. '// ' .. v:sub(min_spaces + 1)
+    end
+    table.insert(res, newline)
   end
-  return newline
+  return res
 end
 
 -- cancel comment
-local function cancel_comment(line)
-  local newline = line:gsub('// ', '')
-  return newline
+local function cancel_comment(lines)
+  local res = {}
+  for _, v in pairs(lines) do
+    local newline = ""
+    if #v > 0 then
+      newline = v:gsub('// ', '')
+    end
+    table.insert(res, newline)
+  end
+  return res
 end
 
 -- generate comment
@@ -24,17 +31,24 @@ function comm.gen_comment(line_start, line_end)
   local lines = fn.getline(line_start, line_end)
 
   local res = {}
+  local has_no_comm = false
+
+  local min_spaces = 300
   for _, v in pairs(lines) do
-    local newline = ""
+    if #v > 0 and v:match('^%s*// ') == nil then
+      has_no_comm = true
+    end
 
     if #v > 0 then
-      if v:match('// ') ~= nil then
-        newline = cancel_comment(v)
-      else
-        newline = add_comment(v)
-      end
+      local _, cur_spaces = v:find('^%s*')
+      min_spaces = cur_spaces < min_spaces and cur_spaces or min_spaces
     end
-    table.insert(res, newline)
+  end
+
+  if has_no_comm == true then
+    res = add_comment(lines)
+  else
+    res = cancel_comment(lines)
   end
 
   return res
