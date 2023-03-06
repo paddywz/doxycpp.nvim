bd57dbc65aafc2428ecf0bdd925054a481ac322a
jerry-1015 <781467164@qq.com>
2023-03-06 18:14:55 +0800

    fix bugs

--- lua/doxycpp/comment.lua
+++ lua/doxycpp/comment.lua
@@ -16,11 +16,13 @@ end
 -- add comment
 local function add_comment(lines, min_spaces)
   local sep = get_sep() .. ' '
+  print(sep)
   local res = {}
   for _, v in pairs(lines) do
     local newline = ""
     if #v > 0 then
       newline = string.rep(' ', min_spaces) .. sep .. v:sub(min_spaces + 1)
+      print(newline)
     end
     table.insert(res, newline)
   end
@@ -44,7 +46,6 @@ end
 -- generate comment
 function comm.gen_comment(line_start, line_end)
   local lines = fn.getline(line_start, line_end)
-  print(get_sep())
 
   local res = {}
   local has_no_comm = false
